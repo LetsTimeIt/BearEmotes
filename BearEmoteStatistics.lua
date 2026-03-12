86c5ec526eb557df8e02503c5f0a86b19232c920
James Loveridge <dev@j-m.es>
2026-03-12 18:58:44 +0000

    chore: update to be midnight compatible

--- BearEmoteStatistics.lua
+++ BearEmoteStatistics.lua
@@ -1,39 +1,77 @@
+local FILTERED_BEAR_SENT_KEYS = {}
+local FILTERED_BEAR_RECEIVED_KEYS = {}
+
+local function GetFallbackBearEmotePath()
+    return "Interface\\AddOns\\BearEmotes\\Emotes\\beargun.tga:28:28"
+end
+
+local function BuildBearStatTextureString(fullEmotePath)
+    local animdata = BearEmotes_animation_metadata[fullEmotePath]
+    if animdata ~= nil then
+        return BearEmotes_BuildEmoteFrameStringWithDimensions(fullEmotePath, animdata, 0, 16, 16)
+    end
+
+    local size = string.match(fullEmotePath, ":(.*)")
+    if size == nil then
+        return "|T" .. fullEmotePath .. ":16:16|t"
+    end
+    return "|T" .. string.gsub(fullEmotePath, size, "16:16") .. "|t"
+end
+
+local function ApplyBearStatsFilter(searchText)
+    if searchText == nil or searchText == "" then
+        FILTERED_BEAR_SENT_KEYS = BearEmoteSentStatKeys
+        FILTERED_BEAR_RECEIVED_KEYS = BearEmoteRecievedStatKeys
+        return
+    end
+
+    local pattern = "^.*" .. searchText:lower() .. ".*"
+
+    FILTERED_BEAR_SENT_KEYS = {}
+    for i = 1, #BearEmoteSentStatKeys do
+        if string.find(BearEmoteSentStatKeys[i]:lower(), pattern) == 1 then
+            FILTERED_BEAR_SENT_KEYS[#FILTERED_BEAR_SENT_KEYS + 1] = BearEmoteSentStatKeys[i];
+        end
+    end
+
+    FILTERED_BEAR_RECEIVED_KEYS = {}
+    for i = 1, #BearEmoteRecievedStatKeys do
+        if string.find(BearEmoteRecievedStatKeys[i]:lower(), pattern) == 1 then
+            FILTERED_BEAR_RECEIVED_KEYS[#FILTERED_BEAR_RECEIVED_KEYS + 1] = BearEmoteRecievedStatKeys[i];
+        end
+    end
+end
+
 function BearStatsScreen_OnLoad()
-    
     BearEmoteSentStatKeys = {}
-    local n = 0;
     local totalEmotesSent = 0;
-    for k,v in pairs(BearEmoteStatistics) do
-        if BearEmotes_defaultpack[k] ~= nil and v[2] > 0 then -- Only add if the emote still exsists
-            n=n+1;
+    for k, v in pairs(BearEmoteStatistics) do
+        if BearEmotes_defaultpack[k] ~= nil and v[2] > 0 then
             totalEmotesSent = totalEmotesSent + v[2];
-            BearEmoteSentStatKeys[n]=k;
+            BearEmoteSentStatKeys[#BearEmoteSentStatKeys + 1] = k;
         end
     end
 
     BearStatsScreenSentListTitle:SetText("Sent " .. totalEmotesSent .. " emotes")
-    
-    --Sort the sent stats list by usage
     table.sort(BearEmoteSentStatKeys, function(left, right)
         return BearEmoteStatistics[left][2] > BearEmoteStatistics[right][2]
     end);
 
     BearEmoteRecievedStatKeys = {}
-    local n = 0;
     local totalEmotesSeen = 0;
-    for k,v in pairs(BearEmoteStatistics) do
-        if BearEmotes_defaultpack[k] ~= nil and v[3] > 0 then -- Only add if the emote still exsists
-            n=n+1;
+    for k, v in pairs(BearEmoteStatistics) do
+        if BearEmotes_defaultpack[k] ~= nil and v[3] > 0 then
             totalEmotesSeen = totalEmotesSeen + v[3];
-            BearEmoteRecievedStatKeys[n]=k;
+            BearEmoteRecievedStatKeys[#BearEmoteRecievedStatKeys + 1] = k;
         end
     end
-    BearStatsScreenSeenListTitle:SetText("Seen " .. totalEmotesSeen .. " emotes")
 
-    --Sort the seen stats list by nr of times seen
+    BearStatsScreenSeenListTitle:SetText("Seen " .. totalEmotesSeen .. " emotes")
     table.sort(BearEmoteRecievedStatKeys, function(left, right)
         return BearEmoteStatistics[left][3] > BearEmoteStatistics[right][3]
     end);
+
+    ApplyBearStatsFilter(BearStatsScreen.searchBox and BearStatsScreen.searchBox:GetText() or "")
 
     BearStatsScreen:SetBackdrop({
         bgFile = 'Interface\\DialogFrame\\UI-DialogBox-Background-Dark',
@@ -46,44 +84,73 @@ function BearStatsScreen_OnLoad()
             right = 0,
             top = 0,
             bottom = 0
-        } 
+        }
     })
 
-    BearStatsScreen.closeButton = CreateFrame('Button', "BearStatsCloseButton", BearStatsScreen, 'UIPanelCloseButtonNoScripts')
-	BearStatsScreen.closeButton:SetScript('OnClick', function() BearStatsScreen:Hide() end)
-	BearStatsScreen.closeButton:SetPoint('TOPRIGHT', 0, 2)
+    if not BearStatsScreen.closeButton then
+        BearStatsScreen.closeButton = CreateFrame('Button', "BearStatsCloseButton", BearStatsScreen, 'UIPanelCloseButtonNoScripts')
+        BearStatsScreen.closeButton:SetScript('OnClick', function() BearStatsScreen:Hide() end)
+        BearStatsScreen.closeButton:SetPoint('TOPRIGHT', 0, 2)
+    end
 
     local engineeringGemTexture = "Interface\\ItemSocketingFrame\\UI-EngineeringSockets";
-    local emoteBorderTex = {tex=engineeringGemTexture, w=43, h=43, left=0.01562500, right=0.68750000, top=0.41210938, bottom=0.49609375, r=1, g=1, b=1, CBx=49, CBy=47, CBLeft=0.01562500, CBRight=0.78125000, CBTop=0.22070313, CBBottom=0.31250000, OBx=49, OBy=47, OBLeft=0.01562500, OBRight=0.78125000, OBTop=0.31640625, OBBottom=0.40820313};
-    local topEmoteBorder = BearStatsScreen:CreateTexture(nil, "BACKGROUND", nil, 1);
-    topEmoteBorder:SetTexture(emoteBorderTex.tex);
-    topEmoteBorder:SetWidth(emoteBorderTex.w * 2);
-    topEmoteBorder:SetHeight(emoteBorderTex.h * 2);
-    topEmoteBorder:SetTexCoord(emoteBorderTex.left, emoteBorderTex.right, emoteBorderTex.top, emoteBorderTex.bottom);
-    topEmoteBorder:SetPoint('CENTER', BearStatsScreen, "TOPLEFT", 128, -108)
-    topEmoteBorder:Show();
+    local emoteBorderTex = {tex = engineeringGemTexture, w = 43, h = 43, left = 0.01562500, right = 0.68750000, top = 0.41210938, bottom = 0.49609375};
 
-    local topSentImagePath = BearEmotes_defaultpack[BearEmoteSentStatKeys[1]] or "Interface\\AddOns\\BearEmotes\\Emotes\\1337.tga";
+    if not BearStatsScreen.topSentEmoteBorder then
+        BearStatsScreen.topSentEmoteBorder = BearStatsScreen:CreateTexture(nil, "BACKGROUND", nil, 1);
+        BearStatsScreen.topSentEmoteBorder:SetTexture(emoteBorderTex.tex);
+        BearStatsScreen.topSentEmoteBorder:SetWidth(emoteBorderTex.w * 2);
+        BearStatsScreen.topSentEmoteBorder:SetHeight(emoteBorderTex.h * 2);
+        BearStatsScreen.topSentEmoteBorder:SetTexCoord(emoteBorderTex.left, emoteBorderTex.right, emoteBorderTex.top, emoteBorderTex.bottom);
+        BearStatsScreen.topSentEmoteBorder:SetPoint('CENTER', BearStatsScreen, "TOPLEFT", 128, -108)
+        BearStatsScreen.topSentEmoteBorder:Show();
+    end
+
+    if not BearStatsScreen.topSeenEmoteBorder then
+        BearStatsScreen.topSeenEmoteBorder = BearStatsScreen:CreateTexture(nil, "BACKGROUND", nil, 1);
+        BearStatsScreen.topSeenEmoteBorder:SetTexture(emoteBorderTex.tex);
+        BearStatsScreen.topSeenEmoteBorder:SetWidth(emoteBorderTex.w * 2);
+        BearStatsScreen.topSeenEmoteBorder:SetHeight(emoteBorderTex.h * 2);
+        BearStatsScreen.topSeenEmoteBorder:SetTexCoord(emoteBorderTex.left, emoteBorderTex.right, emoteBorderTex.top, emoteBorderTex.bottom);
+        BearStatsScreen.topSeenEmoteBorder:SetPoint('CENTER', BearStatsScreen, "TOPLEFT", 384, -108)
+        BearStatsScreen.topSeenEmoteBorder:Show();
+    end
+
+    local topSentImagePath = BearEmotes_defaultpack[BearEmoteSentStatKeys[1]] or GetFallbackBearEmotePath();
     local animdata = BearEmotes_animation_metadata[topSentImagePath]
     BearStatsScreen.topSentEmoteTexture = BearStatsScreen.topSentEmoteTexture or BearStatsScreen:CreateTexture(nil, "BACKGROUND", nil, 2);
-    local topEmoteTexture = BearStatsScreen.topSentEmoteTexture
-
     if animdata ~= nil then
-        topEmoteTexture:SetTexture(topSentImagePath);
-        topEmoteTexture:SetTexCoord(BearEmotes_GetTexCoordsForFrame(animdata, 0)) 
+        BearStatsScreen.topSentEmoteTexture:SetTexture(topSentImagePath);
+        BearStatsScreen.topSentEmoteTexture:SetTexCoord(BearEmotes_GetTexCoordsForFrame(animdata, 0))
     else
         local size = string.match(topSentImagePath, ":(.*)")
         if size then
             topSentImagePath = string.gsub(topSentImagePath, size, "")
         end
-
-        topEmoteTexture:SetTexture(topSentImagePath);
+        BearStatsScreen.topSentEmoteTexture:SetTexture(topSentImagePath);
     end
-    
-    topEmoteTexture:SetWidth(70);
-    topEmoteTexture:SetHeight(70);
-    topEmoteTexture:SetPoint('CENTER', BearStatsScreen, "TOPLEFT", 128, -108)
-    topEmoteTexture:Show();
+    BearStatsScreen.topSentEmoteTexture:SetWidth(70);
+    BearStatsScreen.topSentEmoteTexture:SetHeight(70);
+    BearStatsScreen.topSentEmoteTexture:SetPoint('CENTER', BearStatsScreen, "TOPLEFT", 128, -108)
+    BearStatsScreen.topSentEmoteTexture:Show();
+
+    local topSeenImagePath = BearEmotes_defaultpack[BearEmoteRecievedStatKeys[1]] or GetFallbackBearEmotePath();
+    animdata = BearEmotes_animation_metadata[topSeenImagePath]
+    BearStatsScreen.topSeenEmoteTexture = BearStatsScreen.topSeenEmoteTexture or BearStatsScreen:CreateTexture(nil, "BACKGROUND", nil, 2);
+    if animdata ~= nil then
+        BearStatsScreen.topSeenEmoteTexture:SetTexture(topSeenImagePath);
+        BearStatsScreen.topSeenEmoteTexture:SetTexCoord(BearEmotes_GetTexCoordsForFrame(animdata, 0))
+    else
+        local size = string.match(topSeenImagePath, ":(.*)")
+        if size then
+            topSeenImagePath = string.gsub(topSeenImagePath, size, "")
+        end
+        BearStatsScreen.topSeenEmoteTexture:SetTexture(topSeenImagePath);
+    end
+    BearStatsScreen.topSeenEmoteTexture:SetWidth(70);
+    BearStatsScreen.topSeenEmoteTexture:SetHeight(70);
+    BearStatsScreen.topSeenEmoteTexture:SetPoint('CENTER', BearStatsScreen, "TOPLEFT", 384, -108)
+    BearStatsScreen.topSeenEmoteTexture:Show();
 
     if #BearEmoteSentStatKeys >= 1 and BearEmoteStatistics[BearEmoteSentStatKeys[1]][2] > 0 then
         BearStatsScreenTopSentText:SetText(BearEmoteSentStatKeys[1] .. " sent " .. BearEmoteStatistics[BearEmoteSentStatKeys[1]][2] .. "x")
@@ -97,105 +164,74 @@ function BearStatsScreen_OnLoad()
         BearStatsScreenTopRecievedText:SetText("No emotes seen yet");
     end
 
-    topEmoteBorder = BearStatsScreen:CreateTexture(nil, "BACKGROUND", nil, 1);
-    topEmoteBorder:SetTexture(emoteBorderTex.tex);
-    topEmoteBorder:SetWidth(emoteBorderTex.w * 2);
-    topEmoteBorder:SetHeight(emoteBorderTex.h * 2);
-    topEmoteBorder:SetTexCoord(emoteBorderTex.left, emoteBorderTex.right, emoteBorderTex.top, emoteBorderTex.bottom);
-    topEmoteBorder:SetPoint('CENTER', BearStatsScreen, "TOPLEFT", 384, -108)
-    topEmoteBorder:Show();
-
-    local topSeenImagePath = BearEmotes_defaultpack[BearEmoteRecievedStatKeys[1]] or "Interface\\AddOns\\BearEmotes\\Emotes\\1337.tga";
-    local animdata = BearEmotes_animation_metadata[topSeenImagePath]
-
-    BearStatsScreen.topSeenEmoteTexture = BearStatsScreen.topSeenEmoteTexture or BearStatsScreen:CreateTexture(nil, "BACKGROUND", nil, 2);
-    topEmoteTexture = BearStatsScreen.topSeenEmoteTexture;
-
-    if animdata ~= nil then
-        topEmoteTexture:SetTexture(topSeenImagePath);
-        topEmoteTexture:SetTexCoord(BearEmotes_GetTexCoordsForFrame(animdata, 0)) 
-    else
-        local size = string.match(topSeenImagePath, ":(.*)")
-        if size then
-            topSeenImagePath = string.gsub(topSeenImagePath, size, "")
-        end
-
-        topEmoteTexture:SetTexture(topSeenImagePath);
+    if not BearStatsScreen.searchBox then
+        BearStatsScreen.searchBox = CreateFrame("EditBox", "BearStatsSearchBox", BearStatsScreen, "InputBoxTemplate")
+        BearStatsScreen.searchBox:SetFrameStrata("DIALOG")
+        BearStatsScreen.searchBox:SetSize(150, 16)
+        BearStatsScreen.searchBox:SetAutoFocus(false)
+        BearStatsScreen.searchBox:SetText("")
+        BearStatsScreen.searchBox:SetPoint("TOPLEFT", 55, -185)
+        BearStatsScreen.searchBox:HookScript("OnTextChanged", function(editbox)
+            ApplyBearStatsFilter(editbox:GetText());
+            BearStatsSentScrollBar_Update();
+            BearStatsRecievedScrollBar_Update();
+        end)
     end
 
-    topEmoteTexture:SetWidth(70);
-    topEmoteTexture:SetHeight(70);
-    topEmoteTexture:SetPoint('CENTER', BearStatsScreen, "TOPLEFT", 384, -108)
-    topEmoteTexture:Show();
+    if not BearStatsScreen.entriesInitialized then
+        for i = 2, 17 do
+            local bearStatSentScrollEntry = CreateFrame('Button', "BearStatsSentEntry" .. i, BearStatsScreen, 'BearStatsEntryTemplate')
+            bearStatSentScrollEntry:SetPoint("TOPLEFT", "BearStatsSentEntry" .. (i - 1), "BOTTOMLEFT", 0, 0);
 
-    --Instantiate the scroll list entries
-    for i=2, 17  do
-        local bearStatSentScrollEntry = CreateFrame('Button', "BearStatsSentEntry"..i, BearStatsScreen, 'BearStatsEntryTemplate')
-        bearStatSentScrollEntry:SetPoint("TOPLEFT", "BearStatsSentEntry"..(i-1), "BOTTOMLEFT", 0, 0);
-
-        local bearStatRecievedScrollEntry = CreateFrame('Button', "BearStatsRecievedEntry"..i, BearStatsScreen, 'BearStatsEntryTemplate')
-        bearStatRecievedScrollEntry:SetPoint("TOPLEFT", "BearStatsRecievedEntry"..(i-1), "BOTTOMLEFT", 0, 0);
-        --print("constructed BearStatsSentEntry" .. i .. " (parent BearStatsSentEntry" ..(i-1) )
+            local bearStatRecievedScrollEntry = CreateFrame('Button', "BearStatsRecievedEntry" .. i, BearStatsScreen, 'BearStatsEntryTemplate')
+            bearStatRecievedScrollEntry:SetPoint("TOPLEFT", "BearStatsRecievedEntry" .. (i - 1), "BOTTOMLEFT", 0, 0);
+        end
+        BearStatsScreen.entriesInitialized = true
     end
 
     BearStatsScreen:Show()
     BearStatsSentScrollBar:Show()
     BearStatsRecievedScrollBar:Show()
+    BearStatsSentScrollBar_Update()
+    BearStatsRecievedScrollBar_Update()
 end
 
 function BearStatsSentScrollBar_Update()
     local nrOfItemsVisible = 17
-    local lineplusoffset; -- an index into our data calculated from the scroll offset
-    
-    FauxScrollFrame_Update(BearStatsSentScrollBar,#BearEmoteSentStatKeys,nrOfItemsVisible,16);
-    for line=1, nrOfItemsVisible do
-      lineplusoffset = line + FauxScrollFrame_GetOffset(BearStatsSentScrollBar);
-      if lineplusoffset <= #BearEmoteSentStatKeys then
-        local cEmote = BearEmoteSentStatKeys[lineplusoffset];
-        local fullEmotePath = BearEmotes_defaultpack[cEmote];
-        local animdata = BearEmotes_animation_metadata[fullEmotePath]
-        local texturestr = nil
-        if animdata ~= nil then
-            texturestr = BearEmotes_BuildEmoteFrameStringWithDimensions(fullEmotePath, animdata, 0, 16, 16)
-        else
-            local size = string.match(fullEmotePath, ":(.*)")
-            texturestr = "|T"..string.gsub(fullEmotePath, size, "16:16").."|t"
-        end
+    local filteredStatKeys = FILTERED_BEAR_SENT_KEYS or BearEmoteSentStatKeys
 
-        getglobal("BearStatsSentEntry"..line):SetText("|cFFfce703" .. lineplusoffset ..".|r ".. texturestr .." " .. " |cFF00FF00"..cEmote.."|r sent: " .. BearEmoteStatistics[cEmote][2] .. "x");
-        getglobal("BearStatsSentEntry"..line):Show();
-        
-      else
-        getglobal("BearStatsSentEntry"..line):Hide();
-      end
+    FauxScrollFrame_Update(BearStatsSentScrollBar, #filteredStatKeys, nrOfItemsVisible, 16);
+    for line = 1, nrOfItemsVisible do
+        local lineplusoffset = line + FauxScrollFrame_GetOffset(BearStatsSentScrollBar);
+        if lineplusoffset <= #filteredStatKeys then
+            local cEmote = filteredStatKeys[lineplusoffset];
+            local fullEmotePath = BearEmotes_defaultpack[cEmote];
+            local texturestr = BuildBearStatTextureString(fullEmotePath)
+
+            getglobal("BearStatsSentEntry" .. line):SetText("|cFFfce703" .. lineplusoffset .. ".|r " .. texturestr .. " |cFF00FF00" .. cEmote .. "|r sent: " .. BearEmoteStatistics[cEmote][2] .. "x");
+            getglobal("BearStatsSentEntry" .. line):Show();
+        else
+            getglobal("BearStatsSentEntry" .. line):Hide();
+        end
     end
 end
 
 function BearStatsRecievedScrollBar_Update()
     local nrOfItemsVisible = 17
-    local lineplusoffset; -- an index into our data calculated from the scroll offset
+    local filteredStatKeys = FILTERED_BEAR_RECEIVED_KEYS or BearEmoteRecievedStatKeys
 
-    FauxScrollFrame_Update(BearStatsRecievedScrollBar,#BearEmoteRecievedStatKeys,nrOfItemsVisible,16);
-    for line=1, nrOfItemsVisible do
-      lineplusoffset = line + FauxScrollFrame_GetOffset(BearStatsRecievedScrollBar);
-      if lineplusoffset <= #BearEmoteRecievedStatKeys then
-        local cEmote = BearEmoteRecievedStatKeys[lineplusoffset];
-        local fullEmotePath = BearEmotes_defaultpack[cEmote]
+    FauxScrollFrame_Update(BearStatsRecievedScrollBar, #filteredStatKeys, nrOfItemsVisible, 16);
+    for line = 1, nrOfItemsVisible do
+        local lineplusoffset = line + FauxScrollFrame_GetOffset(BearStatsRecievedScrollBar);
+        if lineplusoffset <= #filteredStatKeys then
+            local cEmote = filteredStatKeys[lineplusoffset];
+            local fullEmotePath = BearEmotes_defaultpack[cEmote]
+            local texturestr = BuildBearStatTextureString(fullEmotePath)
 
-        local animdata = BearEmotes_animation_metadata[fullEmotePath]
-        local texturestr = nil
-        if animdata ~= nil then
-            texturestr = BearEmotes_BuildEmoteFrameStringWithDimensions(fullEmotePath, animdata, 0, 16, 16)
+            getglobal("BearStatsRecievedEntry" .. line):SetText("|cFFfce703" .. lineplusoffset .. ".|r " .. texturestr .. " |cFF00FF00" .. cEmote .. "|r seen: " .. BearEmoteStatistics[cEmote][3] .. "x");
+            getglobal("BearStatsRecievedEntry" .. line):Show();
         else
-            local size = string.match(fullEmotePath, ":(.*)")
-            texturestr = "|T"..string.gsub(fullEmotePath, size, "16:16").."|t"
+            getglobal("BearStatsRecievedEntry" .. line):Hide();
         end
-        
-        getglobal("BearStatsRecievedEntry"..line):SetText("|cFFfce703" .. lineplusoffset ..".|r ".. texturestr .." " .. " |cFF00FF00"..cEmote.."|r seen: " .. BearEmoteStatistics[cEmote][3] .. "x");
-        getglobal("BearStatsRecievedEntry"..line):Show();
-        
-      else
-        getglobal("BearStatsRecievedEntry"..line):Hide();
-      end
     end
 end
--- BearEmotesAnimator.lua
+++ BearEmotesAnimator.lua
@@ -1,13 +1,16 @@
 local BEAREMOTES_TimeSinceLastUpdate = 0
 local BEAREMOTES_T = 0;
+local issecretvalue = issecretvalue or function()
+    return false
+end
 
 function BearEmotesAnimator_OnUpdate(self, elapsed)
 
     if (BEAREMOTES_TimeSinceLastUpdate >= 0.033) then
         -- Update animated emotes in chat windows
-        for i = 1, NUM_CHAT_WINDOWS do
-            for _, visibleLine in ipairs(_G["ChatFrame" .. i].visibleLines) do
-                if(_G["ChatFrame" .. i]:IsShown() and visibleLine.messageInfo ~= BearEmotes_HoverMessageInfo) then 
+        for _, frameName in pairs(CHAT_FRAMES) do
+            for _, visibleLine in ipairs(_G[frameName].visibleLines) do
+                if (_G[frameName]:IsShown() and visibleLine.messageInfo ~= BearEmotes_HoverMessageInfo) then
                     BearEmotesAnimator_UpdateEmoteInFontString(visibleLine, 28, 28);
                 end
             end
@@ -27,69 +30,56 @@ function BearEmotesAnimator_OnUpdate(self, elapsed)
         end
 
         -- Update animated emotes in statistics screen
-        if(BearStatsScreen:IsVisible()) then
-           
-            local topSentImagePath = BearEmotes_defaultpack[BearEmoteSentStatKeys[1]] or "Interface\\AddOns\\BearEmotes\\Emotes\\1337.tga";
+        if (BearStatsScreen:IsVisible()) then
+            local topSentImagePath = BearEmotes_defaultpack[BearEmoteSentStatKeys[1]] or "Interface\\AddOns\\BearEmotes\\Emotes\\beargun.tga:28:28";
             local animdata = BearEmotes_animation_metadata[topSentImagePath:match("(Interface\\AddOns\\BearEmotes\\Emotes.-.tga)")]
-            
-            if(animdata ~= nil) then
+            if (animdata ~= nil) then
                 local cFrame = BearEmotes_GetCurrentFrameNum(animdata)
-                BearStatsScreen.topSentEmoteTexture:SetTexCoord(BearEmotes_GetTexCoordsForFrame(animdata, cFrame)) 
+                BearStatsScreen.topSentEmoteTexture:SetTexCoord(BearEmotes_GetTexCoordsForFrame(animdata, cFrame))
             end
-                
 
-            local topSeenImagePath = BearEmotes_defaultpack[BearEmoteRecievedStatKeys[1]] or "Interface\\AddOns\\BearEmotes\\Emotes\\1337.tga";
-            local animdata = BearEmotes_animation_metadata[topSeenImagePath:match("(Interface\\AddOns\\BearEmotes\\Emotes.-.tga)")]
-            if(animdata ~= nil) then
+            local topSeenImagePath = BearEmotes_defaultpack[BearEmoteRecievedStatKeys[1]] or "Interface\\AddOns\\BearEmotes\\Emotes\\beargun.tga:28:28";
+            animdata = BearEmotes_animation_metadata[topSeenImagePath:match("(Interface\\AddOns\\BearEmotes\\Emotes.-.tga)")]
+            if (animdata ~= nil) then
                 local cFrame = BearEmotes_GetCurrentFrameNum(animdata)
-                BearStatsScreen.topSeenEmoteTexture:SetTexCoord(BearEmotes_GetTexCoordsForFrame(animdata, cFrame)) 
+                BearStatsScreen.topSeenEmoteTexture:SetTexCoord(BearEmotes_GetTexCoordsForFrame(animdata, cFrame))
             end
-            
 
-            for line=1, 17 do
-                local sentEntry = getglobal("BearStatsSentEntry"..line)
-                local recievedEntry = getglobal("BearStatsRecievedEntry"..line)
+            for line = 1, 17 do
+                local sentEntry = getglobal("BearStatsSentEntry" .. line)
+                local recievedEntry = getglobal("BearStatsRecievedEntry" .. line)
 
-                if(sentEntry:IsVisible()) then
+                if (sentEntry:IsVisible()) then
                     BearEmotesAnimator_UpdateEmoteInFontString(sentEntry, 16, 16);
                 end
 
-                if(recievedEntry:IsVisible()) then
+                if (recievedEntry:IsVisible()) then
                     BearEmotesAnimator_UpdateEmoteInFontString(recievedEntry, 16, 16);
                 end
             end
         end
-        
 
         BEAREMOTES_TimeSinceLastUpdate = 0;
     end
 
     BEAREMOTES_T = BEAREMOTES_T + elapsed
-    BEAREMOTES_TimeSinceLastUpdate = BEAREMOTES_TimeSinceLastUpdate +
-                                        elapsed;
+    BEAREMOTES_TimeSinceLastUpdate = BEAREMOTES_TimeSinceLastUpdate + elapsed;
 end
 
 local function escpattern(x)
     return (
-            --x:gsub('%%', '%%%%')
-             --:gsub('^%^', '%%^')
-             --:gsub('%$$', '%%$')
-             --:gsub('%(', '%%(')
-             --:gsub('%)', '%%)')
-             --:gsub('%.', '%%.')
-             --:gsub('%[', '%%[')
-             --:gsub('%]', '%%]')
-             --:gsub('%*', '%%*')
-             x:gsub('%+', '%%+')
-             :gsub('%-', '%%-')
-             --:gsub('%?', '%%?'))
-            )
+        x:gsub('%+', '%%+')
+         :gsub('%-', '%%-')
+    )
 end
 
--- This will update the texture escapesequence of an animated emote
--- if it exsists in the contents of the fontstring
+-- This will update the texture escape sequence of an animated emote
+-- if it exists in the contents of the fontstring.
 function BearEmotesAnimator_UpdateEmoteInFontString(fontstring, widthOverride, heightOverride)
     local txt = fontstring:GetText();
+    if issecretvalue(txt) == true then
+        return
+    end
     if (txt ~= nil) then
         for emoteTextureString in txt:gmatch("(|TInterface\\AddOns\\BearEmotes\\Emotes.-|t)") do
             local imagepath = emoteTextureString:match("|T(Interface\\AddOns\\BearEmotes\\Emotes.-.tga).-|t")
@@ -98,17 +88,18 @@ function BearEmotesAnimator_UpdateEmoteInFontString(fontstring, widthOverride, h
             if (animdata ~= nil) then
                 local framenum = BearEmotes_GetCurrentFrameNum(animdata);
                 local nTxt;
-                if(widthOverride ~= nil or heightOverride ~= nil) then
-                    nTxt = txt:gsub(escpattern(emoteTextureString),
-                                        BearEmotes_BuildEmoteFrameStringWithDimensions(
-                                        imagepath, animdata, framenum, widthOverride, heightOverride))
+                if (widthOverride ~= nil or heightOverride ~= nil) then
+                    nTxt = txt:gsub(
+                        escpattern(emoteTextureString),
+                        BearEmotes_BuildEmoteFrameStringWithDimensions(imagepath, animdata, framenum, widthOverride, heightOverride)
+                    )
                 else
-                    nTxt = txt:gsub(escpattern(emoteTextureString),
-                                      BearEmotes_BuildEmoteFrameString(
-                                        imagepath, animdata, framenum))
+                    nTxt = txt:gsub(
+                        escpattern(emoteTextureString),
+                        BearEmotes_BuildEmoteFrameString(imagepath, animdata, framenum)
+                    )
                 end
 
-                -- If we're updating a chat message we need to alter the messageInfo as wel
                 if (fontstring.messageInfo ~= nil) then
                     fontstring.messageInfo.message = nTxt
                 end
@@ -119,19 +110,25 @@ function BearEmotesAnimator_UpdateEmoteInFontString(fontstring, widthOverride, h
     end
 end
 
-
-
 function BearEmotes_GetAnimData(imagepath)
     return BearEmotes_animation_metadata[imagepath]
 end
 
 function BearEmotes_GetCurrentFrameNum(animdata)
+    if (animdata.pingpong) then
+        local vframen = math.floor((BEAREMOTES_T * animdata.framerate) % ((animdata.nFrames * 2) - 1));
+        if vframen > animdata.nFrames then
+            vframen = animdata.nFrames - (vframen % animdata.nFrames)
+        end
+        return vframen
+    end
+
     return math.floor((BEAREMOTES_T * animdata.framerate) % animdata.nFrames);
 end
 
 function BearEmotes_GetTexCoordsForFrame(animdata, framenum)
-    local fHeight = 1/animdata.nFrames
-    return 0, 1 ,framenum * fHeight, (framenum * fHeight) + fHeight
+    local fHeight = animdata.frameHeight;
+    return 0, 1, framenum * fHeight / animdata.imageHeight, ((framenum * fHeight) + fHeight) / animdata.imageHeight
 end
 
 function BearEmotes_BuildEmoteFrameString(imagepath, animdata, framenum)
@@ -139,22 +136,20 @@ function BearEmotes_BuildEmoteFrameString(imagepath, animdata, framenum)
     local bottom = top + animdata.frameHeight;
 
     local emoteStr = "|T" .. imagepath .. ":" .. animdata.frameWidth .. ":" ..
-                        animdata.frameHeight .. ":0:0:" .. animdata.imageWidth ..
-                        ":" .. animdata.imageHeight .. ":0:" ..
-                        animdata.frameWidth .. ":" .. top .. ":" .. bottom ..
-                        "|t";
+        animdata.frameHeight .. ":0:0:" .. animdata.imageWidth ..
+        ":" .. animdata.imageHeight .. ":0:" ..
+        animdata.frameWidth .. ":" .. top .. ":" .. bottom ..
+        "|t";
     return emoteStr
 end
 
-function BearEmotes_BuildEmoteFrameStringWithDimensions(imagepath, animdata,
-                                                        framenum, framewidth,
-                                                        frameheight)
+function BearEmotes_BuildEmoteFrameStringWithDimensions(imagepath, animdata, framenum, framewidth, frameheight)
     local top = framenum * animdata.frameHeight;
     local bottom = top + animdata.frameHeight;
 
     local emoteStr = "|T" .. imagepath .. ":" .. framewidth .. ":" ..
-                        frameheight .. ":0:0:" .. animdata.imageWidth .. ":" ..
-                        animdata.imageHeight .. ":0:" .. animdata.frameWidth ..
-                        ":" .. top .. ":" .. bottom .. "|t";
+        frameheight .. ":0:0:" .. animdata.imageWidth .. ":" ..
+        animdata.imageHeight .. ":0:" .. animdata.frameWidth ..
+        ":" .. top .. ":" .. bottom .. "|t";
     return emoteStr
 end
